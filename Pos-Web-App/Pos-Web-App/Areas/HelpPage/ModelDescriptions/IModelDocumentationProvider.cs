9f01822fd4cd1d63aac2b1c11faa8a87424de484
Giovanni Villalobos <gjosue253@gmail.com>
2016-06-04 18:01:11 -0600

    Pos-Web-App
    Proyecto de la aplicación web de cobros

---
+++ Pos-Web-App/Pos-Web-App/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace Pos_Web_App.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
