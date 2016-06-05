9841d91644416cd7fb9ebd55b04091c3ce5fefe9
JosafatV <josav09@gmail.com>
2016-06-04 20:46:15 -0600

    updated sql scripts
    Modified schema, added new table, stored procedures, script de llenado

--- Scripts SQL/Llenado.sql
+++ Scripts SQL/Llenado.sql
@@ -1,6 +1,8 @@
 SELECT * FROM EMPLEADO;
 SELECT * FROM SUCURSAL;
 SELECT * FROM ROL;
+SELECT * FROM PROVEEDOR;
+SELECT * FROM PRODUCTO;
 SELECT * FROM EMPLEADO_POR_ROL;
 
 /* Roles */
@@ -19,14 +21,75 @@ INSERT INTO SUCURSAL (Nombre,Direccion,Telefono,Estado) VALUES ('Cartago', 'Cent
 INSERT INTO SUCURSAL (Nombre,Direccion,Telefono, Estado) VALUES ('San José', 'Escazú', 85420136, 'X');
 
 /* Proveedor */
-EXEC sp_insert_proveedor @Nombre=''
-EXEC sp_insert_proveedor @Nombre=''
-EXEC sp_insert_proveedor @Nombre=''
-EXEC sp_insert_proveedor @Nombre=''
+EXEC sp_insert_proveedor @Nombre='Abbott Laboratories';
+EXEC sp_insert_proveedor @Nombre='Johnson & Johnson';
+EXEC sp_insert_proveedor @Nombre='GlaxoSmithKline';
+EXEC sp_insert_proveedor @Nombre='Sanofi-Aventis';
+EXEC sp_insert_proveedor @Nombre='Novartis';
+EXEC sp_insert_proveedor @Nombre='Pfizer';
+
+/* Cliente */
+INSERT INTO CLIENTE (Cedula,Nombre,Apellidos,FechaNacimiento,Estado) VALUES ('01100001','John','Doe','01/01/1990','A')
+INSERT INTO CLIENTE (Cedula,Nombre,Apellidos,FechaNacimiento) VALUES ('323453234', 'Alfred', 'Cen', '1889-01-02', 'Colorado', 'A');
+INSERT INTO CLIENTE (Cedula,Nombre,Apellidos,FechaNacimiento) VALUES ('323453815', 'Kregor', 'Than', '1815-01-02', 'Jericho Reach', 'A');
+INSERT INTO CLIENTE (Cedula,Nombre,Apellidos,FechaNacimiento) VALUES ('323453816', 'Khiron', 'Ruberec', '1820-01-02', 'Karybdis', 'A');
+INSERT INTO CLIENTE (Cedula,Nombre,Apellidos,FechaNacimiento) VALUES ('323453817', 'Meric', 'Voyen', '1785-01-02', 'Barbarus', 'B');
+INSERT INTO CLIENTE (Cedula,Nombre,Apellidos,FechaNacimiento) VALUES ('323453652', 'Fabius', 'Bile', '1525-01-02', 'Chemos', 'A');
+INSERT INTO CLIENTE (Cedula,Nombre,Apellidos,FechaNacimiento) VALUES ('323453818', 'Corbulo', 'Sanguinary', '1230-01-02', 'Baal', 'A');
+INSERT INTO CLIENTE (Cedula,Nombre,Apellidos,FechaNacimiento) VALUES ('323453830', 'Ulrik', 'Lupus', '1526-01-02', 'Fenris', 'A');
+INSERT INTO CLIENTE (Cedula,Nombre,Apellidos,FechaNacimiento) VALUES ('323453832', 'Sternhammer', 'Stormfang', '1702-01-02', 'Fenris', 'A');
+INSERT INTO CLIENTE (Cedula,Nombre,Apellidos,FechaNacimiento) VALUES ('323453833', 'Fabrikus', 'Farbarius', '1562-01-02', 'Terra', 'I');
+INSERT INTO CLIENTE (Cedula,Nombre,Apellidos,FechaNacimiento) VALUES ('323453834', 'Garreon', 'Abnett', '1468-01-02', 'Badab Primaris', 'I');
 
 /* Empleados - Deben existir los roles */
-EXEC sp_insert_Empleado @Contraseña='admin', @Cedula=402260398, @Nombre='Josafat', @Apellidos='Vargas Gamboa', @Rol=1
-EXEC sp_insert_Empleado @Contraseña='admin', @Cedula=402270398, @Nombre='Sebastian', @Apellidos='Gonzalez', @Rol=5
-EXEC sp_insert_Empleado @Contraseña='admin', @Cedula=402280398, @Nombre='Giovanni', @Apellidos='Villalobos', @Rol=5
-EXEC sp_insert_Empleado @Contraseña='admin', @Cedula=402290398, @Nombre='Joseph', @Apellidos='Campos Porras', @Rol=5
+EXEC sp_insert_Empleado @Contraseña='admin', @Cedula=402260398, @Nombre='Josafat', @Apellidos='Vargas Gamboa', @Rol=1;
+EXEC sp_insert_Empleado @Contraseña='admin', @Cedula=402270398, @Nombre='Sebastian', @Apellidos='Gonzalez', @Rol=2;
+EXEC sp_insert_Empleado @Contraseña='admin', @Cedula=402280398, @Nombre='Giovanni', @Apellidos='Villalobos', @Rol=3;
+EXEC sp_insert_Empleado @Contraseña='admin', @Cedula=402290398, @Nombre='Joseph', @Apellidos='Campos Porras', @Rol=4;
 
+/*Cajas - Deben existir sucursales */
+sp_insert_Caja @Dinero=50000.00, @idSucursal=1;
+sp_insert_Caja @Dinero=50000.00, @idSucursal=1;
+	sp_insert_Caja @Dinero=50000.00, @idSucursal=2;
+	sp_insert_Caja @Dinero=50000.00, @idSucursal=2;
+	sp_insert_Caja @Dinero=50000.00, @idSucursal=2;
+sp_insert_Caja @Dinero=50000.00, @idSucursal=3;
+sp_insert_Caja @Dinero=50000.00, @idSucursal=3;
+	sp_insert_Caja @Dinero=50000.00, @idSucursal=4;
+
+/* Productos - Deben existir proveedores*/
+EXEC sp_insert_Producto @Nombre='Acetaminophen', @EAN='556095512398 ', @Precio='', @IdProveedor=1;
+EXEC sp_insert_producto @Nombre='Gravol', @EAN='966713401796 ', @Precio='', @IdProveedor=1;
+EXEC sp_insert_producto @Nombre='Lidocaína', @EAN='312000192190 ', @Precio='', @IdProveedor=1;
+EXEC sp_insert_producto @Nombre='Ibuprofeno', @EAN='883611840295 ', @Precio='', @IdProveedor=2;
+EXEC sp_insert_producto @Nombre='Paracetamol', @EAN='664959343448 ', @Precio='', @IdProveedor=2;
+EXEC sp_insert_producto @Nombre='Alka-Seltzer', @EAN='360123228345 ', @Precio='', @IdProveedor=3;
+EXEC sp_insert_producto @Nombre='Panadol', @EAN='470728100617 ', @Precio='', @IdProveedor=4;
+EXEC sp_insert_producto @Nombre='Yodo', @EAN='845746951938 ', @Precio='', @IdProveedor=5;
+EXEC sp_insert_producto @Nombre='Listerine', @EAN='354541047700 ', @Precio='', @IdProveedor=6;
+EXEC sp_insert_producto @Nombre='Curitas', @EAN='843514793928  ', @Precio='', @IdProveedor=6;
+
+/* Productos por sucursal - Deben existir productos y sucursales */
+EXEC sp_Stock @IdProducto=1, @Stock=50, @StockMinimo=10, @idSucursal=1;
+EXEC sp_Stock @IdProducto=2, @Stock=50, @StockMinimo=10, @idSucursal=1;
+EXEC sp_Stock @IdProducto=3, @Stock=50, @StockMinimo=10, @idSucursal=1;
+EXEC sp_Stock @IdProducto=4, @Stock=50, @StockMinimo=10, @idSucursal=1;
+EXEC sp_Stock @IdProducto=5, @Stock=50, @StockMinimo=10, @idSucursal=1;
+	EXEC sp_Stock @IdProducto=2, @Stock=50, @StockMinimo=10, @idSucursal=2;
+	EXEC sp_Stock @IdProducto=3, @Stock=50, @StockMinimo=10, @idSucursal=2;
+	EXEC sp_Stock @IdProducto=4, @Stock=50, @StockMinimo=10, @idSucursal=2;
+	EXEC sp_Stock @IdProducto=5, @Stock=50, @StockMinimo=10, @idSucursal=2;
+	EXEC sp_Stock @IdProducto=6, @Stock=50, @StockMinimo=10, @idSucursal=2;
+EXEC sp_Stock @IdProducto=6, @Stock=50, @StockMinimo=10, @idSucursal=3;
+EXEC sp_Stock @IdProducto=7, @Stock=50, @StockMinimo=10, @idSucursal=3;
+EXEC sp_Stock @IdProducto=8, @Stock=50, @StockMinimo=10, @idSucursal=3;
+EXEC sp_Stock @IdProducto=9, @Stock=50, @StockMinimo=10, @idSucursal=3;
+EXEC sp_Stock @IdProducto=10, @Stock=50, @StockMinimo=10, @idSucursal=3;
+	EXEC sp_Stock @IdProducto=3, @Stock=50, @StockMinimo=10, @idSucursal=4;
+	EXEC sp_Stock @IdProducto=4, @Stock=50, @StockMinimo=10, @idSucursal=4;
+	EXEC sp_Stock @IdProducto=7, @Stock=50, @StockMinimo=10, @idSucursal=4;
+	EXEC sp_Stock @IdProducto=9, @Stock=50, @StockMinimo=10, @idSucursal=4;
+	EXEC sp_Stock @IdProducto=10, @Stock=50, @StockMinimo=10, @idSucursal=4;
+
+/* Ventas -Deben eistir productos, sucursales, clientes, cajas, pps */
+EXEC sp_insert_venta
--- Scripts SQL/Stored Procedures.sql
+++ Scripts SQL/Stored Procedures.sql
@@ -1,68 +1,85 @@
+/* Inserts a new Caja tuple */
 CREATE PROCEDURE sp_insert_Caja
-	@Dinero money
+	@Dinero money, @idSucursal INT
 	AS
+		DECLARE @IdCaja INT
 		INSERT INTO CAJA (Dinero) VALUES (@Dinero)
+		SELECT @IdCaja = @@IDENTITY
+
+		INSERT INTO CAJA_POR_SUCURSAL (IdCaja,IdSucursal) VALUES (@IdCaja,@IdSucursal)
 	GO
-
-
+	
+/* Inserts a new Cliente tuple */
 CREATE PROCEDURE sp_insert_Cliente
 	@Cedula char(10), @Nombre char(15), @Apellidos char(30), @FechaNacimiento date
 	AS
 		INSERT INTO CLIENTE (Cedula,Nombre,Apellidos,FechaNacimiento) VALUES (@Cedula, @Nombre, @Apellidos, @FechaNacimiento)
 	GO
 
-CREATE PROCEDURE sp_insert_Empleado
-	@Contraseña char(15), @Cedula char(10), @Nombre char(15), @Apellidos char(30), @Rol tinyint
-	AS
-		DECLARE @IdEmpleado int
-		INSERT INTO EMPLEADO (Contraseña,Cedula,Nombre,Apellidos) /*OUTPUT inserted.IdEmpleado*/ VALUES (@Contraseña, @Cedula, @Nombre, @Apellidos)		
-		/* Obtain IdEmpleado from the Empleado just created */
-		SELECT @IdEmpleado = @@IDENTITY
-		/* INSERT INTO Relationship table */
-		INSERT INTO EMPLEADO_POR_ROL (IdRol, IdEmpleado) VALUES (@Rol, @IdEmpleado)
-	GO
-
-CREATE PROCEDURE sp_insert_Producto
-	@Nombre char(15), @EAN char(13), @Precio money, @Stock smallINT, @StockMinimo smallINT, @idSucursal INT
-	AS
-		DECLARE @IdProducto INT
-		INSERT INTO PRODUCTO (Nombre, EAN, Precio, Stock, StockMinimo) VALUES (@Nombre, @EAN, @Precio, @Stock, @StockMinimo)
-		/* Obtain IdEmpleado from the Empleado just created */
-		SELECT @IdProducto = @@IDENTITY
-		/* INSERT INTO Relationship table */
-		INSERT INTO PRODUCTO_POR_SUCURSAL (IdProducto, IdSucursal) VALUES (@IdProducto, @idSucursal)
-	GO
-
+/* Inserts a new Proveedor tuple */
 CREATE PROCEDURE sp_insert_Proveedor
 	@Nombre char(15)
 	AS
 		INSERT INTO PROVEEDOR (Nombre) VALUES (@Nombre)
 	GO
 
+/* Inserts a new Rol tuple */
 CREATE PROCEDURE sp_insert_Rol 
 	@Nombre char(15)
 	AS
 		INSERT INTO ROL (Nombre) VALUES (@Nombre)
 	GO
 
+/* Inserts a new Sucursal tuple */
 CREATE PROCEDURE sp_insert_Sucursal
 	@Nombre char(15), @Direccion char(30), @Telefono INT
 	AS
 		INSERT INTO SUCURSAL (Nombre,Direccion,Telefono) VALUES (@Nombre,@Direccion,@Telefono)
 	GO
 
-CREATE PROCEDURE sp_insert_Venta
-	@IdProduct int, @Quantity smallINT, @IdCaja INT, @IdCliente INT
+/* Inserts a new Producto tuple and it's relationship with Proveedor */
+CREATE PROCEDURE sp_insert_Producto
+	@Nombre char(15), @EAN char(13), @Precio money, @idProveedor INT
 	AS
-		DECLARE @Stock INT, @IdVenta INT
-		/*SELECT @Stock = SELECT P. FROM PRODUCTO AS P WHERE P.IdProducto=@IdProduct*/
+		DECLARE @IdProducto INT
+		INSERT INTO PRODUCTO (Nombre, EAN, Precio) VALUES (@Nombre, @EAN, @Precio)
+		SELECT @IdProducto = @@IDENTITY
+		INSERT INTO PRODUCTO_POR_PROVEEDOR (IdProducto, IdProveedor) VALUES (@IdProducto,@IdProveedor)
+	GO
+
+/* Inserts a new Venta tuple, and its relationships */
+CREATE PROCEDURE sp_insert_Venta
+	@IdProduct int, @Quantity smallINT, @IdCaja INT, @IdCliente INT, @IdSucursal INT
+	AS
+		DECLARE @IdVenta INT
+
 		INSERT INTO VENTA (Timestamp) VALUES (GETDATE())
-		
 		SELECT @IdVenta = @@IDENTITY
 		
 		INSERT INTO VENTA_POR_CAJA (IdVenta, IdCaja) VALUES (@IdVenta,@IdCaja)
 		INSERT INTO VENTA_POR_CLIENTE (IdVenta, IdCliente) VALUES (@IdVenta,@IdCliente)
-		INSERT INTO VENTA_POR_PRODUCTO (IdVenta, IdPRoducto) VALUES (@IdVenta,@IdProduct)
-
-		UPDATE PRODUCTO SET Stock=Stock-@Stock WHERE IdProducto=@IdProduct
+		INSERT INTO PRODUCTO_POR_VENTA(IdVenta, IdPRoducto, Cantidad) VALUES (@IdVenta,@IdProduct,@Quantity)
 	GO
+
+/* Inserts the Productos bought in a Venta and how many and updates the quantity in stock in the Sucursal*/
+CREATE PROCEDURE sp_insert_ProductosPorVenta
+	@IdProducto INT, @IdVenta INT, @Cantidad smallINT, @IdSucursal INT
+	AS
+		INSERT INTO PRODUCTO_POR_VENTA (IdProducto,IdVenta,Cantidad) VALUES (@IdProducto,@IdVenta,@Cantidad)
+		UPDATE PRODUCTO_POR_SUCURSAL SET Stock=Stock-@Cantidad WHERE IdProducto=@IdProducto
+	GO
+
+/* Inserts a Producto into a Sucursal's stock and how many */
+CREATE PROCEDURE sp_Stock
+	@IdProduct INT, @Stock smallINT, @StockMinimo smallINT, @IdSucursal INT
+	AS
+		INSERT INTO PRODUCTO_POR_SUCURSAL (IdProducto,Stock,StockMinimo,IdSucursal) VALUES (@IdProduct,@Stock,@StockMinimo,@IdSucursal)
+	GO
+
+/* Updates the value of a Producto in a Sucursal after restocking */
+CREATE PROCEDURE sp_reStock
+	@IdProduct INT, @Quantity smallINT, @IdSucursal int
+	AS
+		UPDATE PRODUCTO_POR_SUCURSAL SET Stock=Stock+@Quantity WHERE IdProducto=@IdProduct AND IdSucursal=@IdSucursal
+	GO
+
