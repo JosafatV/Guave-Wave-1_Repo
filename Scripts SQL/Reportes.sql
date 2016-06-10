1aa21eeede0d774613c48df5075ed246233f9a0f
JosafatV <josav09@gmail.com>
2016-06-09 21:00:36 -0600

    Update reportes
    Updated reportes, rearranged and created views, modified reportes'
    stored procedures

--- Scripts SQL/Reportes.sql
+++ Scripts SQL/Reportes.sql
@@ -1,24 +1,60 @@
 USE PosPf
 GO
 
-CREATE PROCEDURE report_VentasPorSucursal
-AS
-	SELECT Vps.Nombre AS Sucursal, COUNT(Vps.IdVenta) AS CantidadDeVentas
-	FROM View_VentasPorSucursal AS Vps
-	WHERE Vps.Estado='A'
-	GROUP BY Vps.Nombre
-	GO
+	/* counts the sales for every branch office in a month */
+CREATE PROCEDURE report_VentasPorSucursalPorMes
+	@initialDATE DATETIME
+	AS
+		DECLARE @monthDATE DATETIME
+			SELECT @monthDATE=DATEADD (month, 1, @initialDATE)
 
-CREATE PROCEDURE report_5ProductosMasVendidos
-AS
+	SELECT V.IdSucursal, V.NombreSucursal, V.Direccion, COUNT(V.IdVenta) AS CantidadDeVentas
+	FROM View_Ventas AS V
+	WHERE V.EstadoVenta='A' AND V.Timestamp BETWEEN @initialDATE AND @monthDATE
+	GROUP BY V.IdSucursal, V.NombreSucursal, V.Direccion
+GO
+
+	/* calculate the total profit for every branch office in a month */
+CREATE PROCEDURE report_GananciasGlobalesPorMes
+	@initialDATE DATETIME
+	AS
+		DECLARE @monthDATE DATETIME
+			SELECT @monthDATE=DATEADD (month, 1, @initialDATE)
+
+	SELECT Ppv.IdSucursal, SUM(Cantidad*Precio) AS Ganancias
+	FROM View_ProductoPorVenta AS Ppv
+	WHERE Ppv.Timestamp BETWEEN @initialDATE AND @monthDATE
+	GROUP BY Ppv.IdSucursal
+GO
+
+	/* Selects the top 5 sellers in a branch in a month */
+CREATE PROCEDURE report_ProductosMásVendidosPorSucursalPorMes
+	@initialDATE DATETIME, @IdSucursal INT
+	AS
+		DECLARE @monthDATE DATETIME
+			SELECT @monthDATE=DATEADD (month, 1, @initialDATE)
+
+	SELECT TOP 5 Ppv.Nombre, SUM(Ppv.Cantidad) AS Total, Ppv.NombreSucursal, Ppv.Direccion
+	FROM View_ProductoPorVenta AS Ppv
+	WHERE Ppv.Estado='A' AND Ppv.IdSucursal=@IdSucursal AND  Ppv.Timestamp BETWEEN @initialDATE AND @monthDATE
+	GROUP BY Ppv.Nombre, Ppv.NombreSucursal, Ppv.Direccion
+GO
+
+	/* Selects the top 5 sellers in a month */
+CREATE PROCEDURE report_ProductosMásVendidosGlobalPorMes
+	@initialDATE DATETIME
+	AS
+		DECLARE @monthDATE DATETIME
+			SELECT @monthDATE=DATEADD (month, 1, @initialDATE)
+
 	SELECT TOP 5 Ppv.Nombre, SUM(Ppv.Cantidad) AS Total 
 	FROM View_ProductoPorVenta AS Ppv
-	WHERE Ppv.Estado='A'
+	WHERE Ppv.Estado='A' AND  Ppv.Timestamp BETWEEN @initialDATE AND @monthDATE
 	GROUP BY Ppv.Nombre
-	GO
+GO
 
-/* calculate the sales in a range */
-CREATE PROCEDURE report_VentasInRange
+/* calculate the sales in a range of a branch office */
+CREATE PROCEDURE report_GananciasInRange
 @initialDATE DATETIME, @finalDATE DATETIME, @IdSucursal INT
 AS
 	SELECT SUM(Precio*Cantidad) AS Sale
@@ -48,12 +84,17 @@ AS
 		FROM View_ProductoPorVenta AS Ppv
 		WHERE Ppv.IdSucursal=@IdSucursal AND Ppv.Timestamp BETWEEN @initialDATE AND @monthDATE
 
-	SELECT @dayPROFIT AS Día, @weekPROFIT AS Semana, @monthPROFIT AS Mes
+	SELECT S.Nombre, S.Direccion, @dayPROFIT AS Día, @weekPROFIT AS Semana, @monthPROFIT AS Mes 
+	FROM SUCURSAL AS S
+	WHERE S.IdSucursal=@IdSucursal
 GO
 
 
 
 /* test query for ms checking 
+EXEC report_VentasPorSucursal @initialDATE = '20160601'
+EXEC report_GananciasGlobalesPorMes @initialDATE='20160601'
+
 EXEC sp_VentasInRange @initialDate='2016-06-07 20:10:16', @finalDate='2016-06-07 20:10:16.821'
 EXEC report_Ventas @initialDATE='2016-06-01', @IdSucursal=6
 */
--- Scripts SQL/Views.sql
+++ Scripts SQL/Views.sql
@@ -1,6 +1,7 @@
 USE PosPF;
 GO
 
+/* RELACIONES */
 CREATE VIEW [dbo].[View_CajaPorSucursal]
 AS
 SELECT        dbo.CAJA.IdCaja, dbo.CAJA.Dinero, dbo.CAJA.UltimoCierre, dbo.CAJA.Estado, dbo.SUCURSAL.IdSucursal, dbo.SUCURSAL.Nombre, dbo.SUCURSAL.Direccion, dbo.SUCURSAL.Telefono, dbo.SUCURSAL.Estado AS EstadoSucursal
@@ -8,7 +9,6 @@ FROM            dbo.CAJA INNER JOIN
                          dbo.CAJA_POR_SUCURSAL ON dbo.CAJA.IdCaja = dbo.CAJA_POR_SUCURSAL.IdCaja INNER JOIN
                          dbo.SUCURSAL ON dbo.CAJA_POR_SUCURSAL.IdSucursal = dbo.SUCURSAL.IdSucursal
 Where dbo.CAJA_POR_SUCURSAL.Estado = 'A'
-
 GO
 
 CREATE VIEW [dbo].[View_EmpleadoPorRol]
@@ -31,20 +31,15 @@ FROM            dbo.EMPLEADO INNER JOIN
 Where dbo.EMPLEADO_POR_SUCURSAL.Estado = 'A'
 GO
 
-CREATE VIEW [dbo].[View_ProductoPorVenta]
+CREATE VIEW [dbo].[View_ProductoPorProveedor]
 AS
-SELECT        dbo.PRODUCTO.IdProducto, dbo.PRODUCTO.EAN, dbo.PRODUCTO.Nombre, dbo.PRODUCTO.Precio, dbo.PRODUCTO_POR_VENTA.Cantidad, dbo.PRODUCTO.Estado, dbo.VENTA.Timestamp, dbo.VENTA.IdVenta, 
-                         dbo.CAJA.IdCaja, dbo.SUCURSAL.IdSucursal, dbo.SUCURSAL.Nombre AS NombreSucursal
-FROM            dbo.VENTA INNER JOIN
-                         dbo.PRODUCTO_POR_VENTA INNER JOIN
-                         dbo.PRODUCTO ON dbo.PRODUCTO_POR_VENTA.IdProducto = dbo.PRODUCTO.IdProducto ON dbo.VENTA.IdVenta = dbo.PRODUCTO_POR_VENTA.IdVenta INNER JOIN
-                         dbo.VENTA_POR_CAJA ON dbo.VENTA.IdVenta = dbo.VENTA_POR_CAJA.IdVenta INNER JOIN
-                         dbo.SUCURSAL INNER JOIN
-                         dbo.CAJA INNER JOIN
-                         dbo.CAJA_POR_SUCURSAL ON dbo.CAJA.IdCaja = dbo.CAJA_POR_SUCURSAL.IdCaja ON dbo.SUCURSAL.IdSucursal = dbo.CAJA_POR_SUCURSAL.IdSucursal ON dbo.VENTA_POR_CAJA.IdCaja = dbo.CAJA.IdCaja
-Where dbo.PRODUCTO_POR_VENTA.Estado = 'A'
+SELECT        dbo.PRODUCTO.IdProducto, dbo.PRODUCTO.EAN, dbo.PRODUCTO.Nombre, dbo.PRODUCTO.Precio, dbo.PRODUCTO.Estado, dbo.PROVEEDOR.IdProveedor, dbo.PROVEEDOR.Nombre AS NombreProveedor, 
+                         dbo.PROVEEDOR.Estado AS EstadoProveedor
+FROM            dbo.PRODUCTO INNER JOIN
+                         dbo.PRODUCTO_POR_PROVEEDOR ON dbo.PRODUCTO.IdProducto = dbo.PRODUCTO_POR_PROVEEDOR.IdProducto INNER JOIN
+                         dbo.PROVEEDOR ON dbo.PRODUCTO_POR_PROVEEDOR.IdProveedor = dbo.PROVEEDOR.IdProveedor
+Where dbo.PRODUCTO_POR_PROVEEDOR.Estado = 'A'
 GO
-
 
 CREATE VIEW [dbo].[View_ProductoPorSucursal]
 AS
@@ -56,14 +51,19 @@ FROM            dbo.PRODUCTO INNER JOIN
 Where dbo.PRODUCTO_POR_SUCURSAL.Estado = 'A'
 GO
 
-CREATE VIEW [dbo].[View_ProductoPorProveedor]
+CREATE VIEW [dbo].[View_ProductoPorVenta]
 AS
-SELECT        dbo.PRODUCTO.IdProducto, dbo.PRODUCTO.EAN, dbo.PRODUCTO.Nombre, dbo.PRODUCTO.Precio, dbo.PRODUCTO.Estado, dbo.PROVEEDOR.IdProveedor, dbo.PROVEEDOR.Nombre AS NombreProveedor, 
-                         dbo.PROVEEDOR.Estado AS EstadoProveedor
-FROM            dbo.PRODUCTO INNER JOIN
-                         dbo.PRODUCTO_POR_PROVEEDOR ON dbo.PRODUCTO.IdProducto = dbo.PRODUCTO_POR_PROVEEDOR.IdProducto INNER JOIN
-                         dbo.PROVEEDOR ON dbo.PRODUCTO_POR_PROVEEDOR.IdProveedor = dbo.PROVEEDOR.IdProveedor
-Where dbo.PRODUCTO_POR_PROVEEDOR.Estado = 'A'
+SELECT        dbo.PRODUCTO.IdProducto, dbo.PRODUCTO.EAN, dbo.PRODUCTO.Nombre, dbo.PRODUCTO.Precio, dbo.PRODUCTO_POR_VENTA.Cantidad, dbo.PRODUCTO.Estado, dbo.VENTA.Timestamp, dbo.VENTA.IdVenta, 
+                         dbo.CAJA.IdCaja, dbo.SUCURSAL.IdSucursal, dbo.SUCURSAL.Nombre AS NombreSucursal, dbo.SUCURSAL.Direccion
+FROM            dbo.VENTA INNER JOIN
+                         dbo.PRODUCTO_POR_VENTA INNER JOIN
+                         dbo.PRODUCTO ON dbo.PRODUCTO_POR_VENTA.IdProducto = dbo.PRODUCTO.IdProducto ON dbo.VENTA.IdVenta = dbo.PRODUCTO_POR_VENTA.IdVenta INNER JOIN
+                         dbo.VENTA_POR_CAJA ON dbo.VENTA.IdVenta = dbo.VENTA_POR_CAJA.IdVenta INNER JOIN
+                         dbo.SUCURSAL INNER JOIN
+                         dbo.CAJA INNER JOIN
+                         dbo.CAJA_POR_SUCURSAL ON dbo.CAJA.IdCaja = dbo.CAJA_POR_SUCURSAL.IdCaja ON dbo.SUCURSAL.IdSucursal = dbo.CAJA_POR_SUCURSAL.IdSucursal ON 
+                         dbo.VENTA_POR_CAJA.IdCaja = dbo.CAJA.IdCaja
+WHERE        (dbo.PRODUCTO_POR_VENTA.Estado = 'A')
 GO
 
 CREATE VIEW [dbo].[View_VentaPorCaja]
@@ -85,7 +85,36 @@ FROM            dbo.CLIENTE INNER JOIN
 Where dbo.VENTA_POR_CLIENTE.Estado = 'A'
 GO
 
-<<<<<<< HEAD
+
+/* UTILES */
+
+CREATE VIEW [dbo].[View_Empleados]
+AS
+SELECT        dbo.EMPLEADO.IdEmpleado, dbo.EMPLEADO.Contraseña, dbo.EMPLEADO.Cedula, dbo.EMPLEADO.Nombre, dbo.EMPLEADO.Apellidos, dbo.SUCURSAL.IdSucursal, dbo.SUCURSAL.Nombre AS Sucursal, 
+                         dbo.SUCURSAL.Direccion, dbo.SUCURSAL.Telefono, dbo.ROL.IdRol, dbo.ROL.Nombre AS Rol, dbo.EMPLEADO.Estado, dbo.SUCURSAL.Estado AS EstadoSucursal, dbo.ROL.Estado AS EstadoRol
+FROM            dbo.EMPLEADO INNER JOIN
+                         dbo.EMPLEADO_POR_ROL ON dbo.EMPLEADO.IdEmpleado = dbo.EMPLEADO_POR_ROL.IdEmpleado INNER JOIN
+                         dbo.EMPLEADO_POR_SUCURSAL ON dbo.EMPLEADO.IdEmpleado = dbo.EMPLEADO_POR_SUCURSAL.IdEmpleado INNER JOIN
+                         dbo.ROL ON dbo.EMPLEADO_POR_ROL.IdRol = dbo.ROL.IdRol INNER JOIN
+                         dbo.SUCURSAL ON dbo.EMPLEADO_POR_SUCURSAL.IdSucursal = dbo.SUCURSAL.IdSucursal
+
+GO
+
+CREATE VIEW [dbo].[View_Ventas]
+AS
+SELECT        dbo.VENTA.IdVenta, dbo.VENTA.Timestamp, dbo.VENTA.Estado AS EstadoVenta, dbo.CAJA.IdCaja, dbo.CAJA.Dinero, dbo.CAJA.UltimoCierre, dbo.CAJA.Estado AS EstadoCaja, dbo.SUCURSAL.IdSucursal, 
+                         dbo.SUCURSAL.Nombre AS NombreSucursal, dbo.SUCURSAL.Direccion, dbo.SUCURSAL.Telefono, dbo.SUCURSAL.Estado AS EstadoSucursal, dbo.CLIENTE.IdCliente, dbo.CLIENTE.Cedula, 
+                         dbo.CLIENTE.Nombre AS NombreCliente, dbo.CLIENTE.Apellidos, dbo.CLIENTE.FechaNacimiento, dbo.CLIENTE.Estado AS EstadoCliente
+FROM            dbo.CAJA INNER JOIN
+                         dbo.CAJA_POR_SUCURSAL ON dbo.CAJA.IdCaja = dbo.CAJA_POR_SUCURSAL.IdCaja INNER JOIN
+                         dbo.SUCURSAL ON dbo.CAJA_POR_SUCURSAL.IdSucursal = dbo.SUCURSAL.IdSucursal INNER JOIN
+                         dbo.VENTA_POR_CAJA ON dbo.CAJA.IdCaja = dbo.VENTA_POR_CAJA.IdCaja INNER JOIN
+                         dbo.VENTA ON dbo.VENTA_POR_CAJA.IdVenta = dbo.VENTA.IdVenta INNER JOIN
+                         dbo.VENTA_POR_CLIENTE ON dbo.VENTA.IdVenta = dbo.VENTA_POR_CLIENTE.IdVenta INNER JOIN
+                         dbo.CLIENTE ON dbo.VENTA_POR_CLIENTE.IdCliente = dbo.CLIENTE.IdCliente
+
+GO
+
 CREATE VIEW [dbo].[View_VentaPorClienteYCaja]
 AS
 SELECT        dbo.CLIENTE.IdCliente, dbo.CLIENTE.Cedula, dbo.CLIENTE.Apellidos, dbo.CLIENTE.FechaNacimiento, dbo.CLIENTE.Estado AS EstadoCliente, dbo.CLIENTE.Nombre AS NombreCliente, 
@@ -96,13 +125,3 @@ FROM            dbo.VENTA_POR_CAJA INNER JOIN
                          dbo.CLIENTE ON dbo.VENTA_POR_CLIENTE.IdCliente = dbo.CLIENTE.IdCliente
 Where dbo.VENTA.Estado = 'A'
 GO
-=======
-CREATE VIEW [dbo].[View_VentasPorSucursal]
-AS
-SELECT        dbo.SUCURSAL.Nombre, dbo.VENTA_POR_CAJA.IdVenta, dbo.VENTA_POR_CAJA.Estado
-FROM            dbo.CAJA_POR_SUCURSAL INNER JOIN
-                         dbo.SUCURSAL ON dbo.CAJA_POR_SUCURSAL.IdSucursal = dbo.SUCURSAL.IdSucursal INNER JOIN
-                         dbo.VENTA_POR_CAJA ON dbo.CAJA_POR_SUCURSAL.IdCaja = dbo.VENTA_POR_CAJA.IdCaja
-
-GO
->>>>>>> origin/master
