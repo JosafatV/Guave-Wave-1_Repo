dabfbd157fab925ccbd6351b3931bbf3e101f85f
JosafatV <josav09@gmail.com>
2016-06-12 15:00:38 -0600

    angularJs scripts
    se pusieron a funcionar los elemntod de la vista cajero

--- Pos-Web-App/Pos-Web-App/Scripts/Pos-Scripts/CashierView/cashDeskClosingController.js
+++ Pos-Web-App/Pos-Web-App/Scripts/Pos-Scripts/CashierView/cashDeskClosingController.js
@@ -1,4 +1,7 @@
 angular.module('NigmaBillingApp').controller('cashDeskClosingController', ['$scope', '$routeParams', '$location',
     function ($scope, $routeParams, $location) {
+        $scope.goCashier = function () {
+            $location.path('/NigmaFacturation/CashierView/menuCashier')
+        }
 
     }]);
--- Pos-Web-App/Pos-Web-App/Scripts/Pos-Scripts/CashierView/cashDeskOpeningController.js
+++ Pos-Web-App/Pos-Web-App/Scripts/Pos-Scripts/CashierView/cashDeskOpeningController.js
@@ -1,4 +1,7 @@
 angular.module('NigmaBillingApp').controller('cashDeskOpeningController', ['$scope', '$routeParams', '$location',
     function ($scope, $routeParams, $location) {
+        $scope.goCashier = function () {
+            $location.path('/NigmaFacturation/CashierView/menuCashier')
+        }
 
     }]);
