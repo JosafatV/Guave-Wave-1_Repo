c79f5eb8455958f1d27fd024849d414fadd0c6ba
Giovanni Villalobos <gjosue253@gmail.com>
2016-06-16 21:26:01 -0600

    Modificaciones a la vista cajero
    Se modifico el menu de cajero

--- Pos-Web-App/Pos-Web-App/Scripts/Pos-Scripts/CashierView/menuCashierController.js
+++ Pos-Web-App/Pos-Web-App/Scripts/Pos-Scripts/CashierView/menuCashierController.js
@@ -1,5 +1,5 @@
-﻿angular.module('NigmaBillingApp').controller('menuCashierController', ['$scope', '$routeParams', '$location',
-    function ($scope, $routeParams, $location) {
+﻿angular.module('NigmaBillingApp').controller('menuCashierController', ['$scope', '$routeParams', '$location','waveWebApiResource',
+function ($scope, $routeParams, $location,waveWebApiResource) {
         /*--------------Functions to redirect the user as he/she do something----------------*/
         $scope.goCashDeskOpeningController = function () {
             $location.path('/NigmaFacturation/CashierView/cashDeskOpening');
@@ -9,12 +9,12 @@
         };
         $scope.goSales = function () {
             //Quitar Este:
-            $location.path('/NigmaFacturation/CashierView/sales');
+            //$location.path('/NigmaFacturation/CashierView/sales');
             //Poner Este:
-            /*waveWebApiResource.query({ type: 'ProductoPorSucursal' }).$promise.then(function (data) {
+            waveWebApiResource.query({ type: 'ProductoPorSucursal', extension1: sucursalActual}).$promise.then(function (data) {
                    listaTotal = data;
                    $location.path('/NigmaFacturation/CashierView/sales');
-            });*/
+            });
         };
         $scope.goLogin = function () {
             $location.path('default')
--- Pos-Web-App/Pos-Web-App/Scripts/Pos-Scripts/resourceScript.js
+++ Pos-Web-App/Pos-Web-App/Scripts/Pos-Scripts/resourceScript.js
@@ -1,5 +1,5 @@
 ﻿//Var used to save the URL 
-var urlGeneric = 'http://192.168.0.105';
+var urlGeneric = 'http://192.168.2.11';
 
 //This script is the resource that is used to connect to the web Api od DrPhischel
 angular.module('NigmaBillingApp').factory('waveWebApiResource', function ($resource) {
